ed2ec7a255c27f0502d7e0db5cb6d4b3c71dbb99
Kevin Heritage <55070230+kh14762@users.noreply.github.com>
2020-10-09 12:07:07 -0400

    Added external jars to Module path to set up JavaFX. Added UserLogin.java file

---
+++ BigBonOpsys/module-info.java
@@ -0,0 +1,8 @@
+module javafx {
+	requires javafx.base;
+	requires javafx.media;
+	requires javafx.controls;
+	requires javafx.graphics;
+	
+	exports bigbonapp;
+}
---
+++ BigBonOpsys/src/bigbonapp/UserLogin.java
@@ -0,0 +1,14 @@
+package bigbonapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UserLogin extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		
+	}
+
+}
